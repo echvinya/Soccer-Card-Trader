bec22fbb0ee5a5ba36c05267d6049df5f8ef4965
echvinya <52681910+echvinya@users.noreply.github.com>
2025-06-13 16:22:14 -0400

    Update gameState.js

--- js/core/gameState.js
+++ js/core/gameState.js
@@ -1,1 +1,35 @@
+import { GameConfig } from '../config/gameConfig.js';
+import { GameData } from '../config/gameData.js';
 
+export const GameState = {
+    current: {},
+    market: {},
+    leaderboardScores: [],
+
+    initialize() {
+        this.current = {
+            cash: GameConfig.initialCash,
+            daysRemaining: GameConfig.initialDays,
+            currentLocationId: GameData.locations[0].id,
+            inventory: [],
+            log: [],
+            hasPriceGuide: false,
+            boosterPacksPurchasedToday: 0,
+            showHelpText: true,
+            displayCabinet: [],
+            activeEvents: [],
+            pendingEvent: null,
+            storeDiscount: 0,
+            tempFoundCard: null
+        };
+        this.market = {};
+    },
+
+    getCurrentLocation() {
+        return GameData.locations.find(loc => loc.id === this.current.currentLocationId);
+    },
+
+    getCardDetails(cardId) {
+        return GameData.cards.find(card => card.id === cardId);
+    }
+};
