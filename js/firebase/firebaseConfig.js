917bc1bb5b83ebe6805c3d3bd0ae0cd724275402
echvinya <52681910+echvinya@users.noreply.github.com>
2025-06-13 16:21:09 -0400

    Update firebaseConfig.js

--- js/firebase/firebaseConfig.js
+++ js/firebase/firebaseConfig.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyB_FFY3omc51DkkBNmIftcuy0kiRKuNj1Q",
+    authDomain: "soccer-card-trader.firebaseapp.com",
+    projectId: "soccer-card-trader",
+    storageBucket: "soccer-card-trader.firebasestorage.app",
+    messagingSenderId: "858390637557",
+    appId: "1:858390637557:web:64e33c6339e666b3b94ebc"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
