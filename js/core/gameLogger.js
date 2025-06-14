944725fb85efbb1189bde56d9f219e6e89a1da81
echvinya <52681910+echvinya@users.noreply.github.com>
2025-06-14 00:35:48 -0400

    Update gameLogger.js

--- js/core/gameLogger.js
+++ js/core/gameLogger.js
@@ -1,1 +1,10 @@
+import { GameState } from './gameState.js';
+import { UIRenderer } from '../ui/uiRenderer.js';
 
+export const GameLogger = {
+    addLogMessage(message) {
+        GameState.current.log.unshift(message);
+        if (GameState.current.log.length > 50) GameState.current.log.pop();
+        UIRenderer.renderLog();
+    }
+};
